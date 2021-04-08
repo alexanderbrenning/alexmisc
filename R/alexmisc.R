1fa897818289303ee9aaff0c80474bdde3ede20b
alexanderbrenning <alexander.brenning@uni-jena.de>
2021-04-08 22:39:44 +0200

    package help file

---
+++ R/alexmisc.R
@@ -0,0 +1,10 @@
+#' alexmisc: Alexander Brenning's Miscellaneous Functions
+#'
+#' This package is a collection of miscellaneous utility
+#' functions, e.g. functions to make code more robust (and
+#' the use of try-catch functions more convenient),
+#' utility functions for reading web pages (including
+#' Wikipedia), and some other fun stuff.
+#' @docType package
+#' @name alexmisc
+NULL
